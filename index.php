fd2870b9c73b0b2c82ee75ae7eaa0020ef40c4b5
Kier20 <118508879+Kier20@users.noreply.github.com>
2024-02-16 13:00:49 +0800

    index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="../style/index_style.css">
+    <link rel="stylesheet" type="text/css" href="index_style.css">
     <title>User Login</title>
     <script src="../script/transition.js" defer></script>
 </head>
